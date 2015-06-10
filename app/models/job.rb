9fefad1a3d47c56ae0c4510d86e41da724117fc2
jancidom <jancidom@fit.cvut.cz>
2015-06-10 12:35:24 +0200

    Job error handling

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,8 @@
 class Job < ActiveRecord::Base
   include Adaptorable
 
+  default_scope -> { order(:id_remote) }
+
   belongs_to :scene
   enum state: [ :other, :idle, :running, :completed, :failed ]
   validates :id_remote, presence: true
@@ -12,13 +14,21 @@ class Job < ActiveRecord::Base
   validates_attachment_content_type :result, :content_type => /\Aimage\/.*\Z/
 
   def update_state
-    # return if state == 'completed' || state == 'failed'
+    return if state == 'completed' || state == 'failed'
     job_info = get "jobs/#{id_remote}"
     self.state = job_info['state']
 
     # TODO copy result to public dir when state becomes completed
-    return unless state == 'completed' && result.blank?
-    self.result = File.open(result_path)
+    return unless state == 'completed'
+    begin
+      self.result = File.open(result_path)
+    rescue Errno::EIO
+      # Result file is currently inaccessible, possibly due to being saved
+      self.state = 'other'
+    rescue Errno::ENOENT
+      # No such file, something failed
+      self.state = 'failed'
+    end
     self.save!
   end
 end
